e6eb94d167d8fcfbd524faf1a205c7bb8cb5258f
AchmadZuhriBithealth <achmad.zuhri@bithealth.co.id>
2022-08-23 14:41:09 +0700

    INIT

---
+++ Domain/Model/TR_Api_Hit_Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Model
+{
+    public class TR_Api_Hit_Log
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Thread { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+        public string TimeSpan { get; set; }
+    }
+}
